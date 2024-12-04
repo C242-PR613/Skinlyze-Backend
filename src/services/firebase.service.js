c360dec7faea9cabcf64daeeeead246108a053f2
Muhammad Fauzan Naufal Ridho <101383801+ozannaufal15@users.noreply.github.com>
2024-12-05 03:07:58 +0800

    Fix firebase.service.js

--- src/services/firebase.service.js
+++ src/services/firebase.service.js
@@ -1,4 +1,4 @@
-const { initializeApp, cert } = require("firebase-admin/app");
+const { initializeApp, applicationDefault } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 
 initializeApp({
